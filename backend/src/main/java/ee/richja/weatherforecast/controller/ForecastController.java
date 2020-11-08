b9c6c8f5a1c3e691e832301d7463bf4e43d92e8c
Richard Jalonen <jalonen.richard@gmail.com>
2020-11-08 15:24:14 +0200

    backend: added CORS

--- backend/src/main/java/ee/richja/weatherforecast/controller/ForecastController.java
+++ backend/src/main/java/ee/richja/weatherforecast/controller/ForecastController.java
@@ -3,13 +3,17 @@ package ee.richja.weatherforecast.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import ee.richja.weatherforecast.model.Forecast;
 import ee.richja.weatherforecast.service.ForecastService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
+@CrossOrigin
 @RestController
 public class ForecastController {
     @Autowired
@@ -17,6 +21,7 @@ public class ForecastController {
 
     @GetMapping(value = "/forecast", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Forecast> getWeatherForecast() throws JsonProcessingException {
+        log.info("/forecasts endpoint called");
         return forecastService.getWeatherForecast().getForecast();
     }
 }
